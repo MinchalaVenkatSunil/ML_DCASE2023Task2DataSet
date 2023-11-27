86ece0fd758183493358a77b581ac9a54e126f84
MinchalaVenkatSunil <venkatsunilminchala@gmail.com>
2023-11-28 00:13:28 +0200

    data_collection:simulation.py updates 1. Earlier I was downloading all the attributes files and storing it for further model evaluation. Now I have modified the script logic to collect some random test and train data from the developement set for further use. Need to think of the attributes down the line too 2. Code formatting and clean up. 3. Tested the code locally and it successfully pulls the data from a secured GCP bucket(assumed for now) and segments the audio files as necessary and stores it locally to use it further in the data tranformation.

--- scripts/data_collection_simulation.py
+++ scripts/data_collection_simulation.py
@@ -1,47 +1,100 @@
-import os
+import logging
 from google.cloud import storage
-import pandas as pd
-from datetime import datetime
+import os
+import random
+from datetime import datetime, timedelta
 
-def download_data_from_gcs(bucket_name, source_blob_name, destination_file_path, key_path):
-    """Download a file from Google Cloud Storage."""
-    client = storage.Client.from_service_account_json(key_path)
-    bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
-    blob.download_to_filename(destination_file_path)
+# TODO: Move the logging config from here
+logging.basicConfig(
+    filename='data_collection_simulation.log', 
+    level=logging.DEBUG, 
+    format='%(asctime)s - %(levelname)s - %(message)s'
+    )
 
-def simulate_weekly_data_collection():
-    """
-    Assuming the source data will be available weekly under the secured GCP bucket "dcase2023bucketdataset".
-    Or we can also assume and work with any server or from a url and extract the zip directly
-    Simulates weekly data collection and downloads CSV files from Google Cloud Storage.
-    """
+def download_file(
+        bucket_name, 
+        source_blob_name, 
+        destination_file_name
+        ):
+    """Downloads a file from Google Cloud Storage."""
+    try:
+        json_key_path = "/app/mldocker-4713e7f8b358.json"
+        storage_client = storage.Client.from_service_account_json(json_key_path)
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(source_blob_name)
+        blob.download_to_filename(destination_file_name)
+        logging.info(f"Downloaded file: {source_blob_name} to {destination_file_name}")
+    except Exception as e:
+        logging.error(f"Error downloading file {source_blob_name}: {str(e)}")
 
-    print("Starting the weekly data collection and download script...")
+def list_files(bucket_name, prefix):
+    """Lists all files in a GCS bucket with the given prefix."""
+    try:
+        json_key_path = "/app/mldocker-4713e7f8b358.json"
+        storage_client = storage.Client.from_service_account_json(json_key_path)
+        blobs = storage_client.list_blobs(bucket_name, prefix=prefix)
+        file_names = [blob.name for blob in blobs]
+        logging.info(f"Listed files in {prefix}: {file_names}")
+        return file_names
+    except Exception as e:
+        logging.error(f"Error listing files in {prefix}: {str(e)}")
+        return []
 
-    # Google Cloud Storage configuration
-    bucket_name = os.environ.get("GCS_BUCKET_NAME", "dcase2023bucketdataset")
-    key_path = os.environ.get("GCS_KEY_PATH", "/app/mldocker-4713e7f8b358.json")
+def simulate_weekly_data_collection(
+        bucket_name, 
+        dataset_folder, 
+        output_path
+        ):
+    logging.info("Starting weekly data collection simulation...")
 
-    # Include year, month, and day in the folder structure
-    current_date = datetime.now().strftime("%Y/%m/%d")
-    destination_folder = f'/data/weekly/upload/{current_date}/'
-    os.makedirs(destination_folder, exist_ok=True)
+    os.makedirs(output_path, exist_ok=True)
 
-    # List of machinery types
-    machinery_types = ["fan", "bearing", "gearbox", "slider", "ToyCar", "ToyTrain", "valve"]
+    # List all machine types in the dataset
+    machine_types = list_files(bucket_name, f"{dataset_folder}/")
+    machine_types = list(set(machine_types))
 
-    # Download CSV files for each machinery type
-    for machinery_type in machinery_types:
-        source_blob_name = f"DcaseDevDataSet/{machinery_type}/attributes_00.csv"
-        destination_file_path = os.path.join(destination_folder, f'{machinery_type}_attributes_00.csv')
-        download_data_from_gcs(bucket_name, source_blob_name, destination_file_path, key_path)
-        print(f"Downloaded {machinery_type} data from Google Cloud Storage to {destination_file_path}")
+    logging.info(f"Machine types: {machine_types}")
+    machine_types = [machine_type.split('/')[1] for machine_type in machine_types if '/' in machine_type]
+    machine_types = list(set(machine_types))
 
-    print("Weekly data collection and download script completed.")
+    logging.info(f"Machine types with split: {machine_types}")
+
+    # Simulate weekly data collection for each machine type
+    for machine_type in machine_types:
+        machine_type_prefix = f"{dataset_folder}/{machine_type}"
+
+        logging.info(f"Machine type prefix: {machine_type_prefix}")
+
+        sections = list_files(bucket_name, f"{machine_type_prefix}/")
+        sections = list(set(sections))
+        sections = [section.split('/')[2] for section in sections if '/' in section]
+        sections = list(set(sections))
+
+        for section in sections:
+            section_prefix = f"{machine_type_prefix}/{section}"
+
+            files = list_files(bucket_name, f"{section_prefix}/")
+            files = list(set(files))
+            files = [file.split('/')[-1] for file in files if '/' in file and file.endswith('.wav')]
+            files = list(set(files))
+
+            # Simulate weekly data collection by randomly selecting a subset of files
+            random.shuffle(files)
+            weekly_subset = files[:2] # TODO: Move this adjusting number out
+
+            for file_name in weekly_subset:
+                source_blob_name = f"{section_prefix}/{file_name}"
+                destination_path = os.path.join(output_path, f'{machine_type}_{section}_{file_name}')
+
+                # print(f"Downloading file: {source_blob_name} -> {destination_path}")
+                download_file(bucket_name, source_blob_name, destination_path)
+
+    logging.info("Weekly data collection simulation completed.")
 
 if __name__ == "__main__":
+    # TODO: Create environment or config for these data
+    gcs_bucket_name = "dcase2023bucketdataset"
+    gcs_dataset_folder = "DcaseDevDataSet"
+    output_path = "result/weekly/data_collection"
 
-    destination_file_path = 'app/data/weekly/upload'
-    
-    simulate_weekly_data_collection()
+    simulate_weekly_data_collection(gcs_bucket_name, gcs_dataset_folder, output_path)
