c9c0811bf2cf9617f83de0f126749564d8edf352
MinchalaVenkatSunil <venkatsunilminchala@gmail.com>
2023-11-28 10:33:16 +0200

    data transfer: 1. Adding the locally tested results to git ignore 2. Transfer locally collected data to Google Cloud Storage. 3. The meta data along with it's audio files are collected.
    # TODO:
    1. Create environment or config for these data
    2. Move the logging level config from here
    3. Improvising this logic

--- scripts/data_transfer_to_gcs.py
+++ scripts/data_transfer_to_gcs.py
@@ -1,65 +1,47 @@
-import os
+import logging
 from google.cloud import storage
+import os
 
-def upload_to_gcs(local_file_path, bucket_name, destination_blob_name, key_path):
-    """Upload a file to Google Cloud Storage."""
-    client = storage.Client.from_service_account_json(key_path)
-    bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_filename(local_file_path)
-    print(f"Uploaded: {local_file_path} to {destination_blob_name}")
+# TODO: Move the logging level config from here
+logging.basicConfig(
+    filename='data_transfer_to_gcs.log', 
+    level=logging.DEBUG, 
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-def transfer_data_to_gcs(local_data_folder, bucket_name, key_path):
+def upload_to_gcs(bucket_name, local_path, destination_blob_name):
+    """Uploads a file to Google Cloud Storage."""
+    try:
+        json_key_path = "/app/mldocker-4713e7f8b358.json"
+        # json_key_path = "C:/Users/harit/Documents/Visual Studio 2022/MLDockerTest/ML_DCASE2023Task2DataSet/mldocker-4713e7f8b358.json"
+        storage_client = storage.Client.from_service_account_json(json_key_path)
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(local_path)
+        logging.info(f"Uploaded file: {local_path} to {destination_blob_name}")
+    except Exception as e:
+        logging.error(f"Error uploading file {local_path} to {destination_blob_name}: {str(e)}")
+
+
+def transfer_weekly_data_to_gcs(local_data_path, gcs_bucket_name, gcs_destination_folder):
     """Transfer locally collected data to Google Cloud Storage."""
-    print(f"Starting data transfer from {local_data_folder} to {bucket_name}...")
+    logging.info("Starting weekly data transfer to Google Cloud Storage...")
 
-    # Check if the specified path exists
-    if os.path.exists(local_data_folder):
-        print(f"Contents of the '{local_data_folder}' folder:")
-        
-        # Get the list of files and directories
-        contents = os.listdir(local_data_folder)
-        
-        # Print each item in the list
-        for item in contents:
-            print(item)
-    else:
-        print(f"The folder '{local_data_folder}' does not exist.")
+    local_files = [f for f in os.listdir(local_data_path) if os.path.isfile(os.path.join(local_data_path, f))]
 
-        # Get the list of files and directories
-        contents = os.listdir(os.getcwd())
-        
-        # Print each item in the list
-        for item in contents:
-            print("Items: {item}")
+    for local_file in local_files:
+        local_file_path = os.path.join(local_data_path, local_file)
+        gcs_blob_name = f"{gcs_destination_folder}/{local_file}"
 
-    for root, dirs, files in os.walk(local_data_folder):
-        for file in files:
-            local_file_path = os.path.join(root, file)
+        upload_to_gcs(gcs_bucket_name, local_file_path, gcs_blob_name)
 
-            # destination_blob_name = os.path.relpath(local_file_path, local_data_folder)
-            print(f"\nProcessing: {local_file_path}")
-            # upload_to_gcs(local_file_path, bucket_name, destination_blob_name, key_path)
-
-            # Get the relative path without the starting 'local_data_folder'
-            relative_path = os.path.relpath(local_file_path, local_data_folder)
-
-            # Combine with the desired prefix and replace os.sep with '/'
-            destination_blob_name = f"weekly/upload/{relative_path.replace(os.sep, '/')}"
-
-            # Upload the file to Google Cloud Storage
-            upload_to_gcs(local_file_path, bucket_name, destination_blob_name, key_path)
-
-    print("Data transfer completed.")
+    logging.info("Weekly data transfer to Google Cloud Storage completed.")
 
 if __name__ == "__main__":
-    print("Starting main...")
-    
-    # Set configuration details
-    local_data_folder = "/app/data/weekly/upload/"  # Adjust to the actual local data folder
-    bucket_name = os.environ.get("GCS_BUCKET_NAME", "dcase2023bucketdataset")
-    key_path = os.environ.get("GCS_KEY_PATH", "/app/mldocker-4713e7f8b358.json")
+    # TODO: Create environment or config for these data
+    local_data_path = "result/weekly/data_collection"
+    # local_data_path = "C:/Users/harit/Documents/Visual Studio 2022/MLDockerTest/ML_DCASE2023Task2DataSet/result/weekly/data_collection"
+    gcs_bucket_name = "dcase2023bucketdataset"
+    gcs_destination_folder = "DcaseDevDataSetResult"
 
-    # Transfer data to Google Cloud Storage
-    transfer_data_to_gcs(local_data_folder, bucket_name, key_path)
-
+    transfer_weekly_data_to_gcs(local_data_path, gcs_bucket_name, gcs_destination_folder)
